f6fb53aa0e3d44b56b5839597e67bede82ba0755
AbdElMegeed <AbdElMegeedX7@gmail.com>
2023-03-08 17:34:55 +0400

    66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        convert_digits = int(''.join(map(str, digits))) + 1
+        return [int(digit) for digit in str(convert_digits)]
+
+
+solution = Solution()
+print(solution.plusOne([1, 2, 3]))  # [1,2,4]
+print(solution.plusOne([4, 3, 2, 1]))   # [4,3,2,2]
+print(solution.plusOne([9]))   # [1,0]
